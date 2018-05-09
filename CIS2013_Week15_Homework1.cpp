66b858466e9c6d2e6611b0b63e489b16deaaa3f3
Natasha S Love <debora@inbox.ru>
2018-05-08 19:12:09 -0500

    beginning

--- CIS2013_Week15_Homework1.cpp
+++ CIS2013_Week15_Homework1.cpp
@@ -6,8 +6,30 @@
 
 using namespace std;
 
+Card* get_card(Card** deck, int& last){
+	int n = rand() % 52;
+	Card* c = deck[n];
+	deck[n] = deck[last];
+	deck[last--] = c; //сохраняем адрес карты, чтобы  потом было что чистить
+	return c;
+}
+
 int main(){
+	char over = 0;
+	string name;
 	
+	cout << endl;
+	cout << "Hello!!! " << endl;
+	cout << "Welcome to Black Jack!"<< endl;
+	cout << endl;
+	cout << "What is your name? "<< endl;
+	cin >> name;
+	cout << "Let's play, " << name << endl;
+	
+	
+	
+	
+	/*
 	BlackJack blackjack [52];// 2D array for the deck
 	// вместо создания 2D array - создать класс Card.  
 	// в классе создать конструкторы - присваивают value и suit.. 
@@ -17,6 +39,8 @@ int main(){
 // создать для этого array из 4 char suit [] {'S', 'H', 'C', 'D'}
 // и аrray из 13 char value [] {'2', '3', '4'...'Q', 'K'...}
 // for loop тройная - все arrays перебирает и наполняет
+	
+	*/
 	
 	
 	return 0;
